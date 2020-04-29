cb1b2c49aacc78714c7082fab5eae8f4a1ec29be
Lukas Navašinskas <navasinskaslukas@gmail.com>
2020-04-29 21:55:53 +0300

    Add project files.

---
+++ TUTA_specflow/Features/PostCodeTest.feature
@@ -0,0 +1,65 @@
+﻿Feature: PostCodeTest
+	As a UK citizen
+	I want a postcode lookup
+	Because I want to find out more information about it
+
+Background: 
+	Given I am using the base url 'http://api.postcodes.io/postcodes/' value
+
+Scenario Outline: As a Service I validate values in API Response
+	Given I setup the request to GET using the provided '<postCode>' value
+	When I send the request
+	Then I should receive a response
+	And I should have received a status code of '200' value  
+	And I validate eastings should have '<eastings>' and northings should have '<northings>' values
+	And I validate code of district administration '<code_adminDistrict>' value
+    And I validate Response Object  field '<responseObject>' should have '<ResponseObjectValue>' value
+
+
+	
+	Examples: 
+	| postCode | responseCode | country          | eastings | northings | code_adminDistrict | responseObject | ResponseObjectValue        |
+	| LS3 1EP  | 200          | England          | 429320   | 433751    | E08000035          | nhs_ha         | Yorkshire and the Humber   |
+	| WA2 9QU  | 200          | England          | 360639   | 390679    | E06000007          | nhs_ha         | North West                 |
+	| BT30 8HE | 200          | Northern Ireland | 346246   | 341931    | N09000010          | nhs_ha         | Health & Social Care Board |
+	| SE7 7NR  | 200          | England          | 540987   | 178168    | E09000011          | nhs_ha         | London                     |
+	| SY3 9DX  | 200          | England          | 347535   | 310803    | E06000051          | nhs_ha         | West Midlands              |
+	| ---      | ---          | ---              | ---      | ---       | ---                | ---            | ---                        |
+	| LT-0000  | 404          |                  |          |           |                    | nhs_ha         |                            |
+	| GMFV+7H  | 404          | Ireland          |          |           |                    |                |                            |
+	| LS3 1EP  | 200          | England          | 429320   | 433751    | E08000035          | lsoa           | Leeds 063B                 |
+	| LS3 1EP  | 200          | England          | 429320   | 433751    | E08000035          | msoa           | Leeds 063                  |
+# tasks:
+#	
+#	1) Create an assertion that checks if the returned status code is 200 (use HttpResponseMessage.StatusCode )
+
+#	2) Create an assertion that would validate: country
+#	3) Create an assertion that would validate: eastings, northings
+#	4) Create an assertion that would validate: codes.admin_district
+
+#   
+
+#	5) Complete the examples table with more test cases 
+#		- it MUST contain a negative test
+#		- there should be atleast 4 test cases (this is with the negative test case)
+
+#	6) Add an additional assertion that validates either: nhs_ha, lsoa, msoa
+
+#		e.g. responseObject has the field which will be checked, 
+#		ResponseObjectValue has the value it's tested against 
+
+#(3 different object)
+#	| postCode	| responseObject | ResponseObjectValue		|
+#	| LS3 1EP 	| nhs_ha		 | Yorkshire and the Humber |
+#	| LS3 1EP	| longitude      | Leeds 063B				|
+#	| LS3 1EP	| latitude       | Leeds 063				|  
+
+
+# Notes: 
+# Good postcodes: LS3 1EP, NR34 2PF, OX49 5NU, M32 0JG, NE30 1DP
+
+# The example's first test case has everything filled for tasks 1-5, you still need to add columns for task 6 thou!
+
+# For task 6, is recomendet to use a switch statement rather than multiple nested if statements
+# https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/switch
+# https://www.csharp-examples.net/switch/
---
+++ TUTA_specflow/Steps/PostCodeTestSteps.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System.Net.Http;
+using TUTA_specflow.Entities.JsonObjects;
+using System.Reflection;
+
+namespace TUTA_specflow.Steps
+{
+    [Binding]
+    public class PostCodeTestSteps
+    {
+        public static HttpClient HttpClient { get; private set; }
+        public static HttpRequestMessage HttpRequestMessage { get; private set; }
+        public static HttpResponseMessage HttpResponseMessage { get; private set; }
+        internal static PostCodeObject PostCodeResult { get => PostCodeResult; set => PostCodeResult = value; }
+
+        private static PostCodeObject postCodeResult = new PostCodeObject() { };
+
+        private static string _postCodeUri;
+
+        private int responseCodeValue;
+
+        public static void SwapOutHttpClient(HttpClient client)
+        {
+            HttpClient = client;
+        }
+
+        [BeforeScenario]
+        public void Before()
+        {
+            if (HttpClient == null)
+                HttpClient = new HttpClient();
+
+            if (HttpRequestMessage == null)
+                HttpRequestMessage = new HttpRequestMessage();
+        }
+
+
+        [Given(@"I am using the base url '(.*)' value")]
+        public void GivenIAmUsingTheBaseUrlValue(string baseURL)
+        {
+            if (HttpClient.BaseAddress == null)
+            {
+                HttpClient.BaseAddress = new Uri(baseURL);
+            }
+        }
+
+        [Given(@"I setup the request to GET using the provided '(.*)' value")]
+        public void GivenISetupTheRequestToGETUsingTheProvidedValue(string postCode)
+        {
+            HttpRequestMessage = new HttpRequestMessage();
+            HttpRequestMessage.Method = new HttpMethod("Get");
+            _postCodeUri = postCode;
+        }
+
+        [When(@"I send the request")]
+        public void WhenISendTheRequest()
+        {
+            HttpRequestMessage.RequestUri = new Uri(_postCodeUri, UriKind.Relative);
+            HttpResponseMessage = HttpClient.SendAsync(HttpRequestMessage).Result;
+        }
+
+        [Then(@"I should receive a response")]
+        public void ThenIShouldReceiveAResponse()
+        {
+            string json = @"" + HttpResponseMessage.Content.ReadAsStringAsync().Result + "";
+
+            postCodeResult = JsonConvert.DeserializeObject<PostCodeObject>(json);
+        }
+
+        [Then(@"I should have received a status code of '(.*)' value")]
+        public void ThenIShouldHaveReceivedAStatusCodeOfValue(int responseCode)
+        {
+            Assert.That((int)HttpResponseMessage.StatusCode, Is.EqualTo(responseCode));
+        }
+
+        [Then(@"I validate country should have '(.*)' value")]
+        public void ThenIValidateCountryShouldHaveValue(string country)
+        {
+            Assert.That(postCodeResult.result.country == country);
+        }
+
+        [Then(@"I validate eastings should have '(.*)' and northings should have '(.*)' values")]
+        public void IValidateEastingsShouldHaveAndNorthingsShouldHaveValues(int eastings, int northings)
+        {
+            Assert.That(postCodeResult.result.eastings.Equals(eastings) 
+                         && postCodeResult.result.northings.Equals(northings));
+        }
+
+        [Then(@"I validate code of district administration '(.*)' value")]
+        public void IValidateCodeOfDistrictAdministrationValue(string value)
+        {
+            Assert.That(postCodeResult.result.codes.admin_district.Equals(value));
+        }
+
+        /// <summary>
+        /// Checks if ResponseObject.result has a field with a certaint value
+        /// </summary>
+        /// <param name="fieldName">field name</param>
+        /// <param name="value">field value</param>
+        [Then(@"I validate Response Object  field '(.*)' should have '(.*)' value")]
+        public void IValidateResponseObjectShouldHaveValue(string fieldName, object value)
+        {
+            var itemValue = postCodeResult.result.GetType()
+                                         .GetProperty(fieldName)
+                                         .GetValue(postCodeResult.result);
+            Assert.That(itemValue.Equals(value));
+        }
+    }
+}
